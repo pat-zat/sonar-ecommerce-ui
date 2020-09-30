19a96b97b60348f1e29e875aebe401680f07260b
Patrick Zatolokin <Patrick.Zatolokin@seastarsolutions.com>
2020-09-30 11:42:50 -0500

    backup after file cleanup

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import SimpleReactLightbox from 'simple-react-lightbox';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import './simpleSearch.scss';
 import './ui.scss';
 import './site.css';
@@ -13,7 +12,4 @@ ReactDOM.render(
     <SimpleReactLightbox><App /></SimpleReactLightbox>,
     document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
